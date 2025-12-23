ee9c6e7924f556240e4f899cda5e7791746664e4
regoeee0055 <regoeee0055@gmail.com>
2025-12-23 22:31:05 +0700

    Delete queues directory

--- queues/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Visit, VitalSign, Queue, TriageResult,Device, TelemetryLog
-
-admin.site.register(Visit)
-admin.site.register(VitalSign)
-admin.site.register(Queue)
-admin.site.register(TriageResult)
-admin.site.register(Device)
-admin.site.register(TelemetryLog)
